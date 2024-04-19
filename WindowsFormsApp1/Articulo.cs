2494c27148362c936fe27ffc245400ef0671e0b7
Joaquin <joaquinnoceti@hotmail.com>
2024-04-19 15:24:54 -0300

    clase articulo

---
+++ WindowsFormsApp1/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Articulo
+    {
+        public int CodArticulo { get; set; }
+        public string NombreArticulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public float Precio { get; set; }
+
+        //clase marca
+        //clase categoria
+
+    }
+}
