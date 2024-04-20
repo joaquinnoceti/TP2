8d06df0fa74d47d0f259550271f72f1d26891199
glogatti <logattigustavo@gmail.com>
2024-04-20 13:55:56 -0300

    Creacion clase Categoria y clase Marca

---
+++ WindowsFormsApp1/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Categoria
+    {
+        public int IDCategoria { get; set; }
+        public string Nombre { get; set; }
+    }
+}
