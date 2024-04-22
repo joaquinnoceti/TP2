d5bf65fda667c619db2fe4deee88db46bf18a351
ani_k <ana.lau.villarreal@gmail.com>
2024-04-22 16:07:30 -0300

    ArquitecturaCapas

---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using dominio;
+
+
+
+namespace negocio
+{
+    internal class ArticuloNegocio
+    {
+       
+    }
+}
