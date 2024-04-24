6d43ac0af60f061c09d622de33bbee846ef3b566
ani_k <ana.lau.villarreal@gmail.com>
2024-04-24 20:42:01 -0300

    Agregado PictureBox

--- WindowsFormsApp1/ListarArticulo.Designer.cs
+++ WindowsFormsApp1/ListarArticulo.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.dgvListArticulos = new System.Windows.Forms.DataGridView();
+            this.pbArticulo = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbArticulo)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvListArticulos
             // 
             this.dgvListArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListArticulos.Location = new System.Drawing.Point(144, 101);
+            this.dgvListArticulos.Location = new System.Drawing.Point(27, 65);
             this.dgvListArticulos.Name = "dgvListArticulos";
             this.dgvListArticulos.RowHeadersWidth = 51;
             this.dgvListArticulos.RowTemplate.Height = 24;
-            this.dgvListArticulos.Size = new System.Drawing.Size(505, 223);
+            this.dgvListArticulos.Size = new System.Drawing.Size(698, 284);
             this.dgvListArticulos.TabIndex = 0;
+            this.dgvListArticulos.SelectionChanged += new System.EventHandler(this.dgvListArticulos_SelectionChanged);
+            // 
+            // pbArticulo
+            // 
+            this.pbArticulo.Location = new System.Drawing.Point(770, 65);
+            this.pbArticulo.Name = "pbArticulo";
+            this.pbArticulo.Size = new System.Drawing.Size(269, 284);
+            this.pbArticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbArticulo.TabIndex = 1;
+            this.pbArticulo.TabStop = false;
             // 
             // ListarArticulo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1269, 519);
+            this.Controls.Add(this.pbArticulo);
             this.Controls.Add(this.dgvListArticulos);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ListarArticulo";
@@ -54,6 +67,7 @@
             this.Text = "ListarArticulo";
             this.Load += new System.EventHandler(this.ListarArticulo_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvListArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbArticulo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -61,5 +75,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvListArticulos;
+        private System.Windows.Forms.PictureBox pbArticulo;
     }
 }
--- WindowsFormsApp1/ListarArticulo.cs
+++ WindowsFormsApp1/ListarArticulo.cs
@@ -30,7 +30,7 @@ namespace WindowsFormsApp1
             {
                 listaArticulos = negocio.Listar();
                 dgvListArticulos.DataSource = listaArticulos;
-                
+                pbArticulo.Load(listaArticulos[0].ImagenUrl);
             }
             catch (Exception ex)
             {
@@ -39,6 +39,23 @@ namespace WindowsFormsApp1
             }
         }
 
+        private void dgvListArticulos_SelectionChanged(object sender, EventArgs e)
+        {
+            Articulo seleccionado = (Articulo)dgvListArticulos.CurrentRow.DataBoundItem; 
+            cargarImagen(seleccionado.ImagenUrl);
+        }
 
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbArticulo.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                pbArticulo.Load("https://www.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg");
+            }
+        }
     }
 }
