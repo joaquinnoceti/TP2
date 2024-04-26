19ae5618d108e70ff0f34e05f3f61c2157dda6d0
glogatti <logattigustavo@gmail.com>
2024-04-26 15:14:54 -0300

    Agrego system.ComponentModel para modelar los encabezados de la grilla

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,14 @@ namespace dominio
     public class Articulo
     {   
         public int ID { get; set; }
+        [DisplayName("Código")]
         public string CodArticulo { get; set; }
+        [DisplayName("Nombre")]
         public string NombreArticulo { get; set; }
+        [DisplayName("Descripción")]
         public string Descripcion { get; set; }
         public Marca Marca { get; set; }
+        [DisplayName("Categoría")]
         public Categoria Categoria { get; set; }
         public string ImagenUrl { get; set; }
         public decimal Precio { get; set; }
