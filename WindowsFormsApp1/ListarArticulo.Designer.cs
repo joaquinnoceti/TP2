4b5147b544bb79fd5f2268467201069207fae806
glogatti <logattigustavo@gmail.com>
2024-04-26 11:15:40 -0300

    modificacion de datagridview en listar articulos

--- WindowsFormsApp1/ListarArticulo.Designer.cs
+++ WindowsFormsApp1/ListarArticulo.Designer.cs
@@ -37,31 +37,35 @@
             // dgvListArticulos
             // 
             this.dgvListArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListArticulos.Location = new System.Drawing.Point(27, 65);
+            this.dgvListArticulos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvListArticulos.Location = new System.Drawing.Point(20, 53);
+            this.dgvListArticulos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvListArticulos.MultiSelect = false;
             this.dgvListArticulos.Name = "dgvListArticulos";
             this.dgvListArticulos.RowHeadersWidth = 51;
             this.dgvListArticulos.RowTemplate.Height = 24;
-            this.dgvListArticulos.Size = new System.Drawing.Size(698, 284);
+            this.dgvListArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvListArticulos.Size = new System.Drawing.Size(524, 231);
             this.dgvListArticulos.TabIndex = 0;
             this.dgvListArticulos.SelectionChanged += new System.EventHandler(this.dgvListArticulos_SelectionChanged);
             // 
             // pbArticulo
             // 
-            this.pbArticulo.Location = new System.Drawing.Point(770, 65);
+            this.pbArticulo.Location = new System.Drawing.Point(578, 53);
+            this.pbArticulo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbArticulo.Name = "pbArticulo";
-            this.pbArticulo.Size = new System.Drawing.Size(269, 284);
+            this.pbArticulo.Size = new System.Drawing.Size(202, 231);
             this.pbArticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbArticulo.TabIndex = 1;
             this.pbArticulo.TabStop = false;
             // 
             // ListarArticulo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1269, 519);
+            this.ClientSize = new System.Drawing.Size(952, 422);
             this.Controls.Add(this.pbArticulo);
             this.Controls.Add(this.dgvListArticulos);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ListarArticulo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "ListarArticulo";
