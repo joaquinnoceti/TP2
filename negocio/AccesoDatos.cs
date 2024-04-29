8521c56a0cff7cfab942d4d9c88690d8fc82112c
joaquinnoceti <joaquinnoceti@hotmail.com>
2024-04-28 23:33:39 -0300

    servidor localexpress

--- negocio/AccesoDatos.cs
+++ negocio/AccesoDatos.cs
@@ -21,7 +21,7 @@ namespace negocio
         }
         public AccesoDatos()
         {
-            conexion = new SqlConnection("server=(local); database=CATALOGO_P3_DB; integrated security=true");//server=(local)  server=.\\SQLEXPRESS
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");//server=(local)  server=.\\SQLEXPRESS
             comando = new SqlCommand();
         }
 
